9ed41c904297c1bc45cb50a4b754d71b7a75d7ab
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-05-24 01:51:45 +0900

    Fix migrations

--- EcAuthMigration/EcAuthMigrationDbContext.cs
+++ EcAuthMigration/EcAuthMigrationDbContext.cs
@@ -14,12 +14,12 @@ namespace EcAuthMigration
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=db;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true;MultipleActiveResultSets=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // modelBuilder.RegisterOptionTypes(); // This method is not available in C#. You might need to implement it manually.
+            modelBuilder.Entity<Client>();
         }
     }
 }
