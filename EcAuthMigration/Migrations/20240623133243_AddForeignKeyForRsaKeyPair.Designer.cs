6c833c14233aa71160c6c4350a593b31974fcf56
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-06-23 22:34:28 +0900

    Add RsaKeypairs

--- EcAuthMigration/Migrations/20240623133243_AddForeignKeyForRsaKeyPair.Designer.cs
+++ EcAuthMigration/Migrations/20240623133243_AddForeignKeyForRsaKeyPair.Designer.cs
@@ -4,6 +4,7 @@ using EcAuthMigration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcAuthMigration.Migrations
 {
     [DbContext(typeof(EcAuthMigrationDbContext))]
-    partial class EcAuthMigrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240623133243_AddForeignKeyForRsaKeyPair")]
+    partial class AddForeignKeyForRsaKeyPair
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
