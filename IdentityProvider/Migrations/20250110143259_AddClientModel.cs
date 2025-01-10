913d6e823e60e4509a063ea35052c0fc99def4fb
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-10 23:41:22 +0900

    Add Client

---
+++ IdentityProvider/Migrations/20250110143259_AddClientModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "client",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    client_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    client_secret = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    app_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "client");
+        }
+    }
+}
---
+++ IdentityProvider/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdentityProvider.Models
+{
+    [Table("client")]
+    public class Client
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("client_id")]
+        public string ClientId { get; set; }
+        [Column("client_secret")]
+        public string ClientSecret { get; set; }
+        [Column("app_name")]
+        public string AppName { get; set; }
+        [Column("created_at")]
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        [Column("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
