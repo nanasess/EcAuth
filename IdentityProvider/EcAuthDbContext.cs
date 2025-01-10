0997c58deca4e1ec40307f13802f2d0c4f753a83
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-10 23:41:22 +0900

    Add Client

--- IdentityProvider/EcAuthDbContext.cs
+++ IdentityProvider/EcAuthDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using IdentityProvider.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityProvider
@@ -9,6 +10,7 @@ namespace IdentityProvider
         {
         }
 
+        public DbSet<Client> Clients { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true;MultipleActiveResultSets=true");
