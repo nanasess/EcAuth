43d49623f90f6d2157c886b85d1e0e4d1be5d2ce
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-10 23:19:32 +0900

    Add DbContext

---
+++ IdentityProvider/EcAuthDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityProvider
+{
+    public class EcAuthDbContext : DbContext
+    {
+        public EcAuthDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true;MultipleActiveResultSets=true");
+        }
+    }
+}
