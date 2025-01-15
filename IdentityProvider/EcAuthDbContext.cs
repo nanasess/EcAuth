bfc4d4f3749a2e874869bb3e81ff19dce317d8b2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-16 01:25:21 +0900

    Account と Organization を作成

--- IdentityProvider/EcAuthDbContext.cs
+++ IdentityProvider/EcAuthDbContext.cs
@@ -6,14 +6,16 @@ namespace IdentityProvider
 {
     public class EcAuthDbContext : DbContext
     {
-        public EcAuthDbContext()
+        public EcAuthDbContext(DbContextOptions<EcAuthDbContext> options) : base(options)
         {
         }
 
         public DbSet<Client> Clients { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=localhost;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true;MultipleActiveResultSets=true");
-        }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlServer("Server=db;Database=EcAuthDb;User Id=SA;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true;MultipleActiveResultSets=true");
+        // }
     }
 }
