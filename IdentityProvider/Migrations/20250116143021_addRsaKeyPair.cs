4641a82739a020a02b8e2e0f7585abb96052adbf
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-01-17 23:23:09 +0900

    Add CreateKeyPair

---
+++ IdentityProvider/Migrations/20250116143021_addRsaKeyPair.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRsaKeyPair : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rsa_key_pair",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    public_key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    private_key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    client_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rsa_key_pair", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_rsa_key_pair_client_client_id",
+                        column: x => x.client_id,
+                        principalTable: "client",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rsa_key_pair_client_id",
+                table: "rsa_key_pair",
+                column: "client_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rsa_key_pair");
+        }
+    }
+}
