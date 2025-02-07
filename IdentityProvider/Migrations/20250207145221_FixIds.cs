ccee9f37c4efc632bac538caafe1e2261bf0b8aa
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-07 23:55:47 +0900

    fix(migrations): rename Id columns to id in OpenIdProvider and OpenIdProviderScope tables

---
+++ IdentityProvider/Migrations/20250207145221_FixIds.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "open_id_provider_scope",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "open_id_provider",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "open_id_provider_scope",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "open_id_provider",
+                newName: "Id");
+        }
+    }
+}
