dcb0ee37a9475920d45d9bc9a90b320e228c334a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-09 02:02:45 +0900

    feat(migrations): add Amazon OAuth2 provider and update tenant and client details
    - Added Amazon OAuth2 provider configuration.
    - Updated tenant and client details in the database.
    - Removed unused Google OAuth2 app name variable.
    - Added deletion of open_id_provider entries in the Down method.

---
+++ IdentityProvider/Migrations/20250208165556_UpdateTenantAndClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTenantAndClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            DotNetEnv.Env.TraversePath().Load();
+            var DEFAULT_ORGANIZATION_TENANT_NAME = DotNetEnv.Env.GetString("DEFAULT_ORGANIZATION_TENANT_NAME");
+            var DEFAULT_ORGANIZATION_CODE = DotNetEnv.Env.GetString("DEFAULT_ORGANIZATION_CODE");
+            migrationBuilder.Sql($@"
+                UPDATE organization
+                SET tenant_name = '{DEFAULT_ORGANIZATION_TENANT_NAME}'
+                WHERE code = '{DEFAULT_ORGANIZATION_CODE}'
+            ");
+            var DEFAULT_CLIENT_ID = DotNetEnv.Env.GetString("DEFAULT_CLIENT_ID");
+            var DEFAULT_APP_NAME = DotNetEnv.Env.GetString("DEFAULT_APP_NAME");
+            migrationBuilder.Sql($@"
+                UPDATE client
+                SET app_name = '{DEFAULT_APP_NAME}'
+                WHERE client_id = '{DEFAULT_CLIENT_ID}'
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var DEFAULT_ORGANIZATION_CODE = DotNetEnv.Env.GetString("DEFAULT_ORGANIZATION_CODE");
+            migrationBuilder.Sql($@"
+                UPDATE organization
+                SET tenant_name = NULL
+                WHERE code = '{DEFAULT_ORGANIZATION_CODE}'
+            ");
+            var DEFAULT_CLIENT_ID = DotNetEnv.Env.GetString("DEFAULT_CLIENT_ID");
+            migrationBuilder.Sql($@"
+                UPDATE client
+                SET app_name = NULL
+                WHERE client_id = '{DEFAULT_CLIENT_ID}'
+            ");
+        }
+    }
+}
