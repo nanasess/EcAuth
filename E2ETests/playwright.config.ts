5ddb82f827d0fce03d49feb1ee594bcaff52ecec
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-02-21 01:59:45 +0900

    ci(E2ETests): set output directory for test results in Playwright config

--- E2ETests/playwright.config.ts
+++ E2ETests/playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+  outputDir: 'test-results/',
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
