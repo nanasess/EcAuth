8b1fa193c490b79cdba986d4d6ae00fff314fb8b
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-04-30 08:22:10 +0900

    test: add tests for refresh token flow in authorization code flow

--- E2ETests/tests/specs/mock_idp_authorization_code_flow.spec.ts
+++ E2ETests/tests/specs/mock_idp_authorization_code_flow.spec.ts
@@ -34,6 +34,8 @@ test.describe.serial('認可コードフローのテストをします', () => {
     });
     console.log(await response.json());
     expect((await response.json()).access_token).toBeTruthy();
+    expect((await response.json()).refresh_token).toBeTruthy();
+    const refreshToken = (await response.json()).refresh_token;
     expect((await response.json()).token_type).toBe('Bearer');
 
     const userInfoRequest = await request.newContext();
@@ -45,5 +47,34 @@ test.describe.serial('認可コードフローのテストをします', () => {
 
     console.log(await userInfoResponse.json());
     expect((await userInfoResponse.json()).sub).toBeTruthy();
+
+    const refreshTokenResponse = await tokenRequest.post(tokenEndpoint, {
+      form: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: scopes,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+      }
+    });
+
+    console.log(await refreshTokenResponse.json());
+    expect((await refreshTokenResponse.json()).access_token).toBeTruthy();
+    expect((await refreshTokenResponse.json()).refresh_token).toBeTruthy();
+    expect((await refreshTokenResponse.json()).token_type).toBe('Bearer');
+
+    // TODO: refresh_token が更新されていることを確認する
+    // TODO: 古い access_token でユーザー情報を取得できないことを確認する
+    test.step('Refresh Token で更新したアクセストークンでユーザー情報を取得します', async () => {
+      const refreshTokenUserInfoRequest = await request.newContext();
+      const refreshTokenUserInfoResponse = await refreshTokenUserInfoRequest.get(userInfoEndpoint, {
+        headers: {
+          Authorization: `Bearer ${(await refreshTokenResponse.json()).access_token}`
+        }
+      });
+      console.log(await refreshTokenUserInfoResponse.json());
+      expect((await refreshTokenUserInfoResponse.json()).sub).toBeTruthy();
+      expect((await refreshTokenUserInfoResponse.json()).sub).toBe((await userInfoResponse.json()).sub);
+    });
   });
 });
