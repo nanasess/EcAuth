8bdedd82917adb4172348c3ccd1e185242c74f83
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2025-09-28 08:24:09 +0900

    fix(migrations): update foreign key and remove TempId column
    This commit updates the foreign key constraint on the `access_token` table to reference the `subject` column in the `ecauth_user` table. It also removes the `TempId` column and its associated unique constraint from the `ecauth_user` table.

---
+++ IdentityProvider/Migrations/20250927232321_AddtionalMigrations.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddtionalMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_access_token_ecauth_user_ecauth_subject",
+                table: "access_token");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_ecauth_user_TempId",
+                table: "ecauth_user");
+
+            migrationBuilder.DropColumn(
+                name: "TempId",
+                table: "ecauth_user");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_access_token_ecauth_user_ecauth_subject",
+                table: "access_token",
+                column: "ecauth_subject",
+                principalTable: "ecauth_user",
+                principalColumn: "subject",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_access_token_ecauth_user_ecauth_subject",
+                table: "access_token");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TempId",
+                table: "ecauth_user",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_ecauth_user_TempId",
+                table: "ecauth_user",
+                column: "TempId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_access_token_ecauth_user_ecauth_subject",
+                table: "access_token",
+                column: "ecauth_subject",
+                principalTable: "ecauth_user",
+                principalColumn: "TempId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
